023cec269d37d951052df13c999122240fe4421e
SMOPNIM <144107365+SMOPNIM@users.noreply.github.com>
2026-08-21 20:56:58 +0800

    chore(content): 更改站点URL为：” https://blog.smopnim.de5.net/ “。

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -6,7 +6,7 @@ const SITE_LANG = "zh_CN"; // 语言代码，例如：'en', 'zh_CN', 'ja' 等。
 export const siteConfig: SiteConfig = {
   title: "SMOPNIM's Blog",
   subtitle: "Welcome!",
-  siteURL: "https://mizuki.mysqil.com/", // 请替换为你的站点URL，以斜杠结尾
+  siteURL: "https://blog.smopnim.de5.net/", // 请替换为你的站点URL，以斜杠结尾
   siteStartDate: "2026-08-13", // 站点开始运行日期，用于站点统计组件计算运行天数
   timeZone: "Asia/Shanghai", // 文章日期使用的 IANA 时区，可改为 Asia/Tokyo、Europe/Berlin 等
 
